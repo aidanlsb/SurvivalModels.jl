b0f933d2a584fe9edc3f5b26555773d220d71010
Aidan <aidan@makenotion.com>
2023-11-12 10:02:08 -0800

    better intercept handling

--- src/parametric.jl
+++ src/parametric.jl
@@ -76,7 +76,7 @@ end
 
 
 function survival_function(estimator::AbstractParametricEstimator, ts, params)
-    # this can be zero, which causes numerical errors when we take the log so add eps
+    # this can become zero, which causes numerical errors when we take the log so add eps
     return safe_exp.(-cumulative_hazard(estimator, ts, params)) + 1e-25
 end
 
@@ -92,6 +92,47 @@ function link_to_params(link_funcs, params::Matrix)
     end
     return output
 end
+
+function create_intercept(X)
+    nobs = size(X, 1)
+    intercept = ones(eltype(X), nobs)
+    return hcat(intercept, X)
+end
+
+function initialize_params(estimator::Union{ExponentialEstimator, WeibullEstimator}, ts, e)
+    β0 = zeros(num_params(estimator))
+    β0[end] = log(mean(ts))
+    return β0
+end
+
+
+function initialize_params(estimator::Union{ExponentialEstimator, WeibullEstimator}, ts, e, X)
+    num_predictors = size(X, 2)
+    num_parameters = num_params(estimator)
+    β0 = zeros(num_predictors * num_parameters)  
+    # first coefs are the intercepts for each parameter
+    intercepts = @view β0[1:num_parameters]
+    # for the weibull, initialize the theta parameter to the log mean of durations
+    intercepts[end] = log(mean(ts))
+    return β0
+end
+
+function initialize_params(estimator::MixtureCureEstimator, ts, e)
+    β0_base = initialize_params(estimator.base_estimator, ts, e)
+    β0_c = logit(mean(e))
+    return vcat(β0_c, β0_base)
+end
+
+function initialize_params(estimator::MixtureCureEstimator, ts, e, X)
+    num_params_base = num_params(estimator.base_estimator)
+    β0_base = initialize_params(estimator.base_estimator, ts, e, X)
+    intercepts_base = β0_base[1:num_params_base]
+    coefs_base = β0_base[num_params_base+1:end]
+    intercept_c = logit(mean(e))
+    coef_c = 0
+    return vcat(intercept_c, intercepts_base, coef_c, coefs_base)
+end
+
 
 """
 Compute negative log likelihood for a single observation and set of parameters.
@@ -140,17 +181,13 @@ end
 """ Given a vector of initialized params and X, deal with the shapes and \beta in matrix form."""
 function reshape_params(estimator::AbstractParametricEstimator, X, β)
     num_parameters = num_params(estimator)
-    # add one for the intercept - maybe need to revisit this
-    num_predictors = size(X, 2) + 1
+    num_predictors = size(X, 2)
     return transpose(reshape(β, num_parameters, num_predictors))
 end
 
 function compute_params(estimator::AbstractParametricEstimator, X, params)
     β = reshape_params(estimator, X, params)
-    # add an intercept
-    nobs = size(X, 1)
-    Xb = hcat(ones(nobs), X)
-    return Xb * β
+    return X * β
 end
 
 function neg_log_likelihood(estimator::AbstractParametricEstimator, ts, e, X, params)
@@ -166,36 +203,6 @@ function param_optimization(estimator::AbstractParametricEstimator, obj, x0)
     return Optim.minimizer(res)
 end
 
-# TODO: need to define methods for other estimators, use them here
-function initialize_params(estimator::WeibullEstimator, ts, e)
-    x0 = zeros(num_params(estimator))
-    x0[end] = log(mean(ts))
-    return x0
-end
-
-# TODO: finish this - init for weibull regression
-# function initialize_params(estimator::WeibullEstimator, ts, e, X)
-#     num_predictors = size(X, 2)
-#     num_parameters = num_params(estimator)
-# end
-
-function initialize_params(estimator::MixtureCureEstimator, ts, e)
-    x0_base = initialize_params(estimator.base_estimator, ts, e)
-    x0_c = logit(mean(e))
-    return vcat(x0_c, x0_base)
-end
-
-# TODO: improve this, need to reference base estimator
-function initialize_params(estimator::MixtureCureEstimator, ts, e, X)
-    num_predictors = size(X, 2)
-    num_parameters = num_params(estimator)
-    intercepts = zeros(num_parameters)
-    intercepts[1] = logit(mean(e))
-    intercepts[end] = log(mean(ts))
-    β = zeros(num_predictors * (num_parameters))
-    return vcat(intercepts, β)
-end
-
 function fit(estimator::AbstractParametricEstimator, ts, e)
     obj(β) = neg_log_likelihood(estimator, ts, e, β)
     β0 = initialize_params(estimator, ts, e)
@@ -204,9 +211,15 @@ function fit(estimator::AbstractParametricEstimator, ts, e)
 end
 
 
-function fit(estimator::AbstractParametricEstimator, ts, e, X)
-    obj(β) = neg_log_likelihood(estimator, ts, e, X, β)
-    β0 = initialize_params(estimator, ts, e, X)
+function fit(estimator::AbstractParametricEstimator, ts, e, X; add_intercept=true)
+    if add_intercept
+        X_input = create_intercept(X)
+    else
+        X_input = X
+    end
+
+    obj(β) = neg_log_likelihood(estimator, ts, e, X_input, β)
+    β0 = initialize_params(estimator, ts, e, X_input)
     β = param_optimization(estimator, obj, β0)
     return FittedParametricEstimator(estimator, β)
 end
@@ -228,10 +241,15 @@ function confint(fitted::FittedParametricEstimator, ts, e; confidence_level=0.95
     return (lower=lower, upper=upper)
 end
 
-function confint(fitted::FittedParametricEstimator, ts, e, X; confidence_level=0.95)
+function confint(fitted::FittedParametricEstimator, ts, e, X; confidence_level=0.95, add_intercept=true)
+    if add_intercept
+        X_input = create_intercept(X)
+    else
+        X_input = X
+    end
     βhat = get_params(fitted) 
     estimator = estimator_from_fitted(fitted)
-    nll(β) = neg_log_likelihood(estimator, ts, e, X, β)
+    nll(β) = neg_log_likelihood(estimator, ts, e, X_input, β)
     H = ForwardDiff.hessian(x -> nll(x), βhat)
     variance = inv(H)
     se = sqrt.(diag(variance))
